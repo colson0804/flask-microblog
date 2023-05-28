ea3162f7640ed9371ca32f1011fba548dd9b9389
Craig Olson <craig.olson804@gmail.com>
2023-05-28 14:18:48 -0700

    simple blog template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Craig'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        }, 
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was just okay!'
+        }
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
