e5c7ce5c7fc9e2dfaad65a60c01d9198b3bd02a6
Craig Olson <craig.olson804@gmail.com>
2023-05-30 15:18:12 -0700

    Reset password email notification

--- app/models.py
+++ app/models.py
@@ -2,7 +2,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
 from hashlib import md5
-from app import login, db
+from time import time
+import jwt
+from app import app, login, db
 
 @login.user_loader 
 def load_user(id):
@@ -56,6 +58,19 @@ class User(UserMixin, db.Model):
         own = Post.query.filter_by(user_id=self.id)
         
         return followed.union(own).order_by(Post.timestamp.desc())
+    
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+    
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
