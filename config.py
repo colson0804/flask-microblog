1a601f996fd2c2427a42b7d711e6a4cfb1dc7048
Craig Olson <craig.olson804@gmail.com>
2023-05-30 10:15:13 -0700

    Add pagination to index and explore pages

--- config.py
+++ config.py
@@ -14,3 +14,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['craig.olson804@gmail.com']
+    # Pagination
+    POSTS_PER_PAGE = 3
